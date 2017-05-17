b3e8f626aaf2c05fd7593f90d6732c20f401d3a7
AlexaInga2 <alexainga1@gmail.com>
2017-05-17 07:34:21 +0000

    a-z + 10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * print_alphabet_x10 - prints alphabet 10 times
+ */
+void print_alphabet_x10(void)
+{
+	char az = 'a';
+	int i = 0;
+
+	while ( i <= 10 )
+	{
+		while ( az <= 'z' )
+		{
+			_putchar(az);
+			az++;
+		}	
+		i++;	
+		_putchar('\n');
+	}
+}	
