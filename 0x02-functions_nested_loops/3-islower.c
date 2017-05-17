d2a84d11f760432e3962c087371b25ab809b4830
AlexaInga2 <alexainga1@gmail.com>
2017-05-17 09:32:44 +0000

    islower and return function

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * is_lower - Check lowercase letters and  is greater than 0
+ * @c: The number to be checked
+ *
+ * Return: 1 if the number is positive. 0 otherwise
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (0);
+	else 
+		return (1);
+}
