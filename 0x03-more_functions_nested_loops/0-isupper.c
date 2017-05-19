e95a1243f3c35b4ef473af1b395736680e7ed07a
AlexaInga2 <alexainga1@gmail.com>
2017-05-19 03:34:21 +0000

    more functions

--- 0x03-more_functions_nested_loops/0-isupper.c
+++ 0x03-more_functions_nested_loops/0-isupper.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 /**
- * i_supper - Check for a uppercase letter
- * @c: The character to be checked
+ * i_supper - check for a uppercase letter
+ * @c: the character to be checked
  * Return: 1 if the character c falls into the tested class, 0 if not
  */
 int _isupper(int c)
