57c5c5558e4f741dd3bc6e70ff5dabe0f884d02c
AlexaInga2 <alexainga1@gmail.com>
2017-05-23 20:36:31 +0000

    questions 1&2, pointers

---
+++ 0x04-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * reset_to_98 - updates the value to 98
+ * @n: The number to be checked
+ *
+ * Return: protoype calls for a void, so no return
+ */
+void reset_to_98(int *n) /** pointer variable decleration*/
+{
+		 *n = 98; /** assigning or declaring pointer value */
+}
