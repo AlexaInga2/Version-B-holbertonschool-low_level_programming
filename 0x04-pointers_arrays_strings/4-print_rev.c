57d02593da17961601dd14fca36812ea642f0043
AlexaInga2 <alexainga1@gmail.com>
2017-05-24 21:58:42 +0000

    changes to top message and betty

--- 0x04-pointers_arrays_strings/4-print_rev.c
+++ 0x04-pointers_arrays_strings/4-print_rev.c
@@ -3,7 +3,7 @@
 /**
  * print_rev - reverse the quote
  * @s: The string
- * @a: we need another variable to subtract i (the string)to reverse
+ *
  * Return: quote in reverse
  */
 void print_rev(char *s)
