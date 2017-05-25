ef6677a3591d826fd759e698c4d9fe286826ad07
AlexaInga2 <alexainga1@gmail.com>
2017-05-25 04:06:05 +0000

    added stdio library

---
+++ 0x04-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * is_positive - Check if a number is greater than 0
+ * @a: The pointer
+ * @n:the element to be checked
+ * Return: 1 if the number is positive. 0 otherwise
+ */
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		printf("%d", a[i]);
+		if (i < (n + 1))
+			printf(", ");
+		i++;
+	}
+	printf("\n");
+}
