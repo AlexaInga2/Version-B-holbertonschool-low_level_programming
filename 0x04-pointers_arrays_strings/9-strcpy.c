47a1a5e44309a22f3a1f223e1f09946944bccbb7
AlexaInga2 <alexainga1@gmail.com>
2017-05-25 04:25:01 +0000

    ugh why

---
+++ 0x04-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * _strcpy - copies string
+ * @dest: destination
+ * @src: source
+ * Return: Always 0.
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+	{
+		i != '\0';
+	while ((dest[i] = src[i]))
+		i++;
+	}
+}
