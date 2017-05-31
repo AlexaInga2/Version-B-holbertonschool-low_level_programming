dfa7ead764b48d1cafd2eb8da0825d119c027ad9
AlexaInga2 <alexainga1@gmail.com>
2017-05-31 21:41:15 +0000

    function that sets value to a pointer to a char

---
+++ 0x06-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * op_add - Makes the sum of two numbers
+ * @arg1: First operand
+ * @arg2: Second operand
+ *
+ * Return: The sum of the two parameters
+ */
+void print_diagsums(int *a, int size)
+{
+	int i, j;
+
+	
+
+}
