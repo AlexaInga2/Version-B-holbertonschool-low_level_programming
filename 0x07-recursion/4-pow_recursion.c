79df75b4e6d916f01c6b7645276a2dae39fceb80
AlexaInga2 <alexainga1@gmail.com>
2017-06-13 00:41:36 +0000

    function that returns the value of x and raised to y

--- 0x07-recursion/4-pow_recursion.c
+++ 0x07-recursion/4-pow_recursion.c
@@ -2,7 +2,7 @@
 /**
  * _pow_recursion - prints a string followed by a new line
  * @x: string to print
- * @y
+ * @y: raising the power
  * Return: -1 if y < 0
  */
 int _pow_recursion(int x, int y)
