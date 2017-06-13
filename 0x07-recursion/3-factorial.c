54df6ec9c3720849d47b26eb98fbf9911868ee32
AlexaInga2 <alexainga1@gmail.com>
2017-06-13 00:36:13 +0000

    function that returns the factorial of given #

--- 0x07-recursion/3-factorial.c
+++ 0x07-recursion/3-factorial.c
@@ -1,6 +1,5 @@
 #include "holberton.h"
 /**
-<<<<<<< HEAD
  *  factorial - returns factorial of given #
  * @n: integer to find factorial
  *
@@ -13,18 +12,4 @@ int factorial(int n)
 	if (n == 0)
 		return (1);
 	return (n * factorial(n - 1));
-=======
- * factorial - returns factorial of given #
- * @n: integer to find factorial
- *
- * Return: factorial of n, -1 if n<0, 1 if n==0
- */
-int factorial(int n)
-{
-  if (n < 0)
-    return (-1);
-  if (n == 0)
-    return (1);
-  return (n * factorial(n-1));
->>>>>>> d7aa629a3d163fab19058268169c164edb033a1b
 }
