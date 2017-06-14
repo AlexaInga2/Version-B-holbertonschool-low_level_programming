933536820605f4c3c0c5dcd85db9ebad13978cb4
AlexaInga2 <alexainga1@gmail.com>
2017-06-14 05:36:57 +0000

    natural sqrt of number

--- 0x07-recursion/5-sqrt_recursion.c
+++ 0x07-recursion/5-sqrt_recursion.c
@@ -1,8 +1,32 @@
 #include "holberton.h"
 /**
- * _sqrt_recursion - prints a string followed by a new line
- * @n: string to print
- *
+ * _sqrt_recursion - prints the natural sqrt of a #
+ * @n: the natural number
+ * @s: the sqare root of natural number
+ * @calcsqrt: function for establishing 0 as starting point for sqrt
  * Return: -1 if n ! have natural sq root
  */
 int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+}
+
+/**
+ * calcsqrt - finds the natural sqrt of a #
+ * @n: the natural number
+ * @s: the square root of natural number
+ * @calcsqrt: function for establishng 0 as starting point for sqrt
+ * Return: s and s + 1 if s = n
+ */
+int calcsqrt(int s, int n)
+{
+	int (calcsqrt(0, n));
+
+	if (s * s > n)
+		return (-1);
+	if (s * s == n)
+		return (s);
+	return (calcsqrt(s + 1, n));
+
+}
