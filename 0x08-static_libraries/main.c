fe65a26663ce724e7f1e4587d56ff0ef9cd2eda5
AlexaInga2 <alexainga1@gmail.com>
2017-06-17 19:07:50 +0000

    static library creation

---
+++ 0x08-static_libraries/main.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+/**
+ * main - intranet check for static library 
+ * Return : 0 which means its true
+ */
+int main(void)
+{
+    _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+    return (0);
+}
