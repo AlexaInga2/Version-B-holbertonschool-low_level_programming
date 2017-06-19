2da0b5524354485ccd18c8248f7f16a0d7280fe6
AlexaInga2 <alexainga1@gmail.com>
2017-06-19 23:41:26 +0000

    3 print numbers

---
+++ 0x0F-variadic_functions/3-print_all.c
@@ -0,0 +1,19 @@
+#include "variadic_functions.h"
+/**
+ * print_all - function that prints anything
+ * @format: list of types of arguments passed to function
+ * @char:
+ *
+ * Return:
+ */
+void print_all(const char * const format, ...)
+{
+
+	char c;
+	int i;
+	float f;
+ 	char* s = "(nil)"
+	va_list args;
+		format = var
+
+			} va_arg( a
