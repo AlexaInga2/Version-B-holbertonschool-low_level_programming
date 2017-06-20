8388ff6ed8d291a70573ca72a1c11728c89ed62c
AlexaInga2 <alexainga1@gmail.com>
2017-06-20 00:33:17 +0000

    printing anything

--- 0x0F-variadic_functions/3-print_all.c
+++ 0x0F-variadic_functions/3-print_all.c
@@ -9,11 +9,52 @@
 void print_all(const char * const format, ...)
 {
 
-	char c;
-	int i;
-	float f;
- 	char* s = "(nil)"
-	va_list args;
-		format = var
+}
 
-			} va_arg( a
+/**
+ * print_c - prints characters
+ * @c: characters to print
+ *
+ * Return: void
+ */
+void print_c(va_list c)
+{
+
+}
+
+
+/**
+ * print_s - prints strings
+ * @s: string to print
+ *
+ * Return: void, or (nil) if string is NULL
+ */
+void print_s(va_list s)
+{
+
+}
+
+
+/**
+ * print_i - print integers
+ * @i: int to print
+ *
+ * Return: void
+ */
+void print_i(va_list i)
+{
+
+}
+
+
+
+/**
+ * print_f: print floats
+ * @f: the float to print
+ *
+ *Return: void
+ */
+void print_f(va_list f)
+{
+
+}
