a72c2eafd2d321a1487fefa01b337309a8fa93fa
AlexaInga2 <alexainga1@gmail.com>
2017-06-23 21:45:26 +0000

    fixed conversion specifier

--- 0x11-singly_linked_lists/0-print_list.c
+++ 0x11-singly_linked_lists/0-print_list.c
@@ -15,7 +15,7 @@ size_t print_list(const list_t *h)
 	temp = h;
 	for (i = 0; temp; i++)
 	{
-		printf("[%i] %s\n", temp->len, temp->str);
+		printf("[%u] %s\n", temp->len, temp->str);
 		temp = temp->next;
 	}
 	return (i);
