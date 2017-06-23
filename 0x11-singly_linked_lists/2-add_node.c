519dcd9df6acd8cb1fbaccb14f057e5b00cb484e
AlexaInga2 <alexainga1@gmail.com>
2017-06-23 23:38:21 +0000

    test

--- 0x11-singly_linked_lists/2-add_node.c
+++ 0x11-singly_linked_lists/2-add_node.c
@@ -11,13 +11,14 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
-  list_t *new;
+	list_t *new;
 
-  if (str == NULL)
-    return (NULL);
-  new = malloc(sizeof(list_t));
-  if (new == NULL)
-    return (NULL);
-  
+	if (str == NULL)
+		return (NULL);
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+
+	*head = temp;
 
 }
