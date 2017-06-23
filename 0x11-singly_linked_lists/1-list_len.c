74b02fb2170c6e62a9d29b986e1affe33f822f33
AlexaInga2 <alexainga1@gmail.com>
2017-06-23 06:52:29 +0000

    find element of linked list

---
+++ 0x11-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * list_len - find the number of elements n a linked list
+ * @h: pointer to the start of list
+ *
+ * Return: number of elements in linked list
+ */
+size_t list_len(const list_t *h)
+{
+  const list_t *temp;
+  unsigned int i;
+
+  temp = h;
+  unsigned int i;
+
+  temp = h;
+  for (i = 0; temp; i++)
+    temp = temp->next;
+  return (i);
+
+}
