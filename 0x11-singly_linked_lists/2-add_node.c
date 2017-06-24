fdbe90a13b71f6f3a51e25d1fb36644b8d7d9a7d
AlexaInga2 <alexainga1@gmail.com>
2017-06-24 00:43:49 +0000

    adding new node to beginning of list

--- 0x11-singly_linked_lists/2-add_node.c
+++ 0x11-singly_linked_lists/2-add_node.c
@@ -11,34 +11,14 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
-<<<<<<< HEAD
 	list_t *new;
 
-	if (str == NULL)
-		return (NULL);
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
 		return (NULL);
-
-	*head = temp;
-
-=======
-  list_t *new;
-  
-  if (str == NULL)
-    return (NULL);
-  new = malloc(sizeof(list_t));
-  if (new == NULL)
-    return (NULL);
-  new->str = strdup(str);
-  if (new->str == NULL)
-    {
-      free(new);
-      return (NULL);
-    }
-  new->len = _strlen(new->str);
-  new->next = *head;
-  *head->new;
-  return (new);
->>>>>>> 0535c21b72af9a1935daf19ccb28c527ae1194d8
+	new->str = strdup(str);
+	new->len = strlen(new->str);
+	new->next = *head;
+	*head = new;
+	return (new);
 }
