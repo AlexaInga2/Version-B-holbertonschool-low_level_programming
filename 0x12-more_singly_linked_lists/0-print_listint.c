bd9e5ed36577133fb91bc36c85617ab6aabb564e
AlexaInga2 <alexainga1@gmail.com>
2017-06-27 00:39:42 +0000

    function prints all elements in list

---
+++ 0x12-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_listint - prints all elements of listint_t list
+ * @h:  pointer to the first spot/start of the list
+ *
+ * Return: the number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t numb;
+
+	numb = 0;
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		numb++;
+	}
+	return (numb);
+}
