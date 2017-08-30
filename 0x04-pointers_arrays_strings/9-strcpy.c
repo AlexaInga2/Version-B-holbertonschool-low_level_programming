75aae8897a161483759b0cf7e84dc8b592c1e233
AlexaInga2 <alexainga1@gmail.com>
2017-08-30 09:38:04 +0000

    writing function using strcpy

--- 0x04-pointers_arrays_strings/9-strcpy.c
+++ 0x04-pointers_arrays_strings/9-strcpy.c
@@ -1,16 +1,20 @@
 #include "holberton.h"
 /**
- * _strcpy - copies string
- * @dest: destination
- * @src: source
- * Return: Always 0.
+ * _strcpy - copies string from src, w/ ('\0'), to buffer pointed to by dest.
+ * @dest: destination string pointer
+ * @src: source string pointer
+ *
+ * Return: pointer to dest
  */
 char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
 	{
-		i != '\0';
+
 	while ((dest[i] = src[i]))
 		i++;
 	}
+	return (dest);
 }
+
+/** function _strcpy already includes terminating null byte (\0) so dn't add */
