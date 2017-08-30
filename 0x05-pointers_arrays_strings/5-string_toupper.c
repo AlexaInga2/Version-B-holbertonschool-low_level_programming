e210b18d5071e56a7d6d30e4b1a67cea6d0aa799
AlexaInga2 <alexainga1@gmail.com>
2017-08-30 13:39:52 +0000

    function that changes all lowercase letters of a string to uppercase

--- 0x05-pointers_arrays_strings/5-string_toupper.c
+++ 0x05-pointers_arrays_strings/5-string_toupper.c
@@ -1,2 +1,19 @@
 #include "holberton.h"
-char *string_toupper(char *)
+/**
+ * string_toupper - changes all lowercase letters of a string to uppercase
+ * @s: string to convert
+ *
+ * Return: the pointer to s (address)
+ */
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+			s[i] -= 'a' - 'A';
+		i++;
+	}
+	return (s);
+}
